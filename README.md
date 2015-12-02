56ef07d5b1851cf5f9e1d80e3b559c4b694476e5
sushant12 <sushatnbajracharya@gmail.com>
2015-12-02 06:16:39 +0545

    updated readme and documented code

--- README.md
+++ README.md
@@ -3,4 +3,12 @@
 Creates necessary vhost file and installs your project
 
 #License
-Screw it, Do whatever you want with it and remember to have fun. 
+Screw it, Do whatever you want with it and remember to have fun.
+
+#Install
+
+You need to have ruby installed.
+
+Clone the repo. Cd into the repo and run `sudo ruby alchi_vhost.rb`, that`s all.
+
+If you want to configure the settings , open the `alchi_vhost.rb` file and start editing.
--- alchi_vhost.rb
+++ alchi_vhost.rb
@@ -1,21 +1,24 @@
 #sudo rm livingunx.com fuitter.sushant.com me.demo1.sushant.com unx.sushant.com teamunx.sushant.com u.sushant.com sus.com sushant.com
-#put the link to your host file
 require 'fileutils'
+#put the link to your host file
 open('/etc/hosts') do |f|
   matches = []
   vhosts = []
   f.readlines.each do |lines|
     matches << lines if lines =~ /.*.com/
   end
+  #depending upon your vhost you may need to configure here
   matches.each do |val|
     val.split.each do |x|
       vhosts << x if x =~ /.*.com/
     end
   end
   vhosts.each do |domain|
-    unless File.file? "/etc/#{domain}"
-      File.symlink  "/etc/#{domain}", "/opt/#{domain}"
-      open("/etc/#{domain}", 'w') do |g|
+    #configure the directory of your sites-available
+    unless File.file? "/etc/nginx/sites-available/#{domain}"
+      #include path to sites-available and sites-enabled directory
+      File.symlink  "/etc/nginx/sites-available/#{domain}", "/etc/nginx/sites-enabled/#{domain}"
+      open("/etc/nginx/sites-available/#{domain}", 'w') do |g|
         g << "server { \n"
         g << "\tlisten 80 default_server;\n"
         g << "\tlisten [::]:80 default_server ipv6only=on;\n"
@@ -35,6 +38,7 @@ open('/etc/hosts') do |f|
         g << "\tpassenger_enabled on;\n"
         g << "}\n"
       end
+      #set the correct path to your web access directory
       %x(rails new /usr/share/nginx/html/"#{domain}")
     end
   end
